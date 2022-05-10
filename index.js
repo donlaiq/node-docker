a723e3107ac470cdd7eccd9f1860b186d7763eea
donlaiq <donlaiq@gmail.com>
2022-05-10 15:08:03 -0300

    adf

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use(session({
 app.use(express.json());
 
 app.get("/api/v1", (req, res) => {
-    res.send("<h2>Hi there!!!!</h2>");
+    res.send("<h2>Hi there</h2>");
     console.log("yeah it ran");
 });
 
